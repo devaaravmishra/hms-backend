8a7e79e2524d622de7432d75ab58f637bfb96f2a
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-26 11:39:41 +0530

    Add App.js & package.json

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = 7000;
+const DBPORT = 27017;
+
+mongoose
+  .connect(`mongodb://localhost:${DBPORT}/hms`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.info(`Connected to database at PORT: ${DBPORT}`);
+  })
+  .catch(() => {
+    console.error(`Error connecting to database at PORT: ${DBPORT}`);
+  });
+
+app.listen(PORT, () => {
+  console.info(`Server is running on PORT: ${PORT}`);
+});
