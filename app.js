4dffd7e2b19059767afff8b3c0b964994666def6
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-27 19:56:27 +0530

    User cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 const adminRoute = require("./routes/adminRoute");
 const userRoute = require("./routes/userRoute");
