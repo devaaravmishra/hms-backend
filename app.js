ca0aa5c5cadf89105d63b0e896a925256dd87f05
Aarav619 <aaravmishra619@gmail.com>
2021-11-30 12:08:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ mongoose
 app.use("/admin", adminRoute);
 app.use("/user", userRoute);
 
-app.post("/", (req, res) => {
-  res.send(200).send("Hello World!")
-})
+app.use("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
 
 module.exports = app;
